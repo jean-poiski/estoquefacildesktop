f0d3c56b57da5efc3d74d86c65f70e18a57f4953
Jean Poiski <jpoiski@gmail.com>
2019-09-24 21:25:42 -0300

    Commit inicial

---
+++ principal.pas
@@ -0,0 +1,26 @@
+unit Principal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfoPrincipal = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  foPrincipal: TfoPrincipal;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
